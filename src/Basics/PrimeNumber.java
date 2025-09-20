44fbb943579a7f703de135cc17d1964a9679c227
Parneet Saxena <parneetsaxena@gmail.com>
2025-09-20 18:27:35 +0530

    Core Progress

---
+++ src/Basics/PrimeNumber.java
@@ -0,0 +1,11 @@
+package Basics;
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+
+    }
+}
+

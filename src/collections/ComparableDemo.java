f344b7256f9f65c5cf02a5e7cdb8c51983a277b6
Parneet Saxena <parneetsaxena@gmail.com>
2026-07-01 04:21:03 +0530

    Comparable & Comparator

--- src/collections/ComparableDemo.java
+++ src/collections/ComparableDemo.java
@@ -1,6 +1,23 @@
+/*
+Comparable is an interface which sorts inside the class.
+Only one parameter is provided.
+The method is implemented inside the class
+compareTo method is used to sort.
+The method compares the current instance with the parametric instance using this keyword.
+
+
+Method to work with comparable
+- implement it with the class (implements Comparable<ClassName>)
+- create the method compareTo(ClassName cn)
+- write the logic for the specified sorting
+- Use Collections.sort() in the main method
+- The result will be sorted using the specified Comparable.
+ */
+
+
 package collections;
 import java.util.*;
-public class compare  {
+public class ComparableDemo {
 
     public static void main(String[] args) {
         List<Students> stud = new ArrayList<>();
--- src/collections/ComparatorDemo.java
+++ src/collections/ComparatorDemo.java
@@ -2,6 +2,15 @@
 Comparator is an interface used to compare between two values.
 It is an interface and cannot be instantiated directly.
 It allows custom sorting techniques. The user can manipulate the sorting process according to the needs
+
+Method to use Comparator
+- Either implement it with the class or use anonymous class
+- Use Collections.sort() method
+- The arguments in the method will be (collection, new Comparator<ClassName>(){});
+- Create the compare method in the anonymous class
+- The method will take two parameters
+- Write the sorting logic
+- The result will be sorted
  */
 
 
@@ -13,7 +22,7 @@ package collections;
 
 import java.util.Comparator;
 import java.util.*;
-public class Comparators implements Comparator<Integer> {
+public class ComparatorDemo implements Comparator<Integer> {
 
     @Override
     public int compare(Integer o1, Integer o2) {
@@ -32,7 +41,7 @@ public class Comparators implements Comparator<Integer> {
         al.add(51);
 
 
-        Comparator<Integer> comp = new Comparators();
+        Comparator<Integer> comp = new ComparatorDemo();
         al.sort(comp);
 
         for(int i:al){

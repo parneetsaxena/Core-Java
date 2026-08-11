c2e2dc18eaf9e63d62cc40d4e0027fbda6458dd3
Parneet Saxena <parneetsaxena@gmail.com>
2026-08-12 02:03:02 +0530

    Refactored Problems

--- src/foundations/problems/Dice.java
+++ src/foundations/problems/Dice.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Dice {
     public static void main(String[] args) {
         Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
         System.out.println("The dice rolls "+ random.nextInt(1,7)+ " for you..");
     }
 }
--- src/foundations/problems/RectangleFormulas.java
+++ src/foundations/problems/RectangleFormulas.java
@@ -4,12 +4,15 @@ public class RectangleFormulas {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int l,b,area,perimeter;
+
         System.out.println("Enter the length: ");
         l = scanner.nextInt();
         System.out.println("Enter the breadth: ");
         b = scanner.nextInt();
+
         area = l*b;
-        System.out.println("The area of rectangle is: "+ area);
+        System.out.println("The area of is: "+ area);
+
         perimeter = 2*(l+b);
         System.out.println("The perimeter is: "+ perimeter);
     }
